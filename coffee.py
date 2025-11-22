de344922e0ec93d417d8e91d778d1bb786a407bb
Verah Mokaya <verahmokaya475@gmail.com>
2025-11-23 02:36:04 +0300

    created instance attirbutes, proprty and name setter to check the details customer is inputing to be string andmore than 3 characters

--- coffee.py
+++ coffee.py
@@ -4,3 +4,23 @@ MENU = [
 {"name": "Latte", "price": 250}
 ]
 
+from typing import List, Optional
+
+class Coffee:
+    all_coffees: List['Coffee'] = []
+
+    def __init__(self, name: str):
+        self._name = None
+        self.name = name  # Trigger setter for validation
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        if not isinstance(value, str) or len(value) < 3:
+            raise ValueError("Coffee name must be a string with at least 3 characters.")
+        self._name = value
+
+    # relationship helpers
